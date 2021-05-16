7cdaffd94848b1c1f539476bcdf829632e919ba9
Adrian <adrian_zinko@op.pl>
2021-05-16 10:41:53 +0200

    Add custom modal

---
+++ src/components/CustomModal/CustomModal.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+import Typography from "@material-ui/core/Typography";
+
+import useStyles from "./styles";
+
+const CustomModal = ({ show, hide, content }) => {
+  const classes = useStyles();
+
+  return (
+    <>
+      {show && (
+          <Dialog
+            maxWidth="md"
+            onClose={hide}
+            aria-labelledby="customized-dialog-title"
+            open={show}
+          >
+            <DialogTitle id="customized-dialog-title" onClose={hide}>
+              {content.title}
+            </DialogTitle>
+            <DialogContent dividers>
+              <img
+                src={content.selectedFile}
+                className={classes.modalImage}
+                alt={content.title}
+              />
+            </DialogContent>
+            <DialogContent dividers>
+              <Typography gutterBottom>
+              <iframe width="100%" height="500" src={content.snippetUrl} allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0" title={content.title}></iframe>
+              </Typography>
+            </DialogContent>
+            <DialogActions>
+              <Button autoFocus onClick={hide} color="primary">
+                Close
+              </Button>
+            </DialogActions>
+          </Dialog>
+      )}
+    </>
+  );
+};
+
+export default CustomModal;
